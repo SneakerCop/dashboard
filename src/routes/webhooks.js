9b3e57fcb5d1aca2433d9cc531ff1a641af12d65
Peter Soboyejo <thepcmrtim@gmail.com>
2019-03-11 23:04:55 -0400

    wtf

--- src/routes/webhooks.js
+++ src/routes/webhooks.js
@@ -11,6 +11,7 @@ const router = express.Router();
 router.post('/stripe', (req, res) => {
     const errMsg = 'Invalidd Input, this endpoint only accepts verified cancellation requests.';
     console.log('webhook req called');
+    console.log(req)
     console.log(req.body);
     stripe.events.retrieve(req.body.id, (err, event) => {
         if (err) return res.status(400).json({
