23d0de8bd0182fb6f461aedf42cac7a395af95fc
Peter Soboyejo <thepcmrtim@gmail.com>
2019-03-11 23:00:49 -0400

    whoops

--- src/routes/webhooks.js
+++ src/routes/webhooks.js
@@ -8,26 +8,25 @@ const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY);
 
 const router = express.Router();
 
-export default () => {
-    router.post('/stripe', (req, res) => {
-        const errMsg = 'Invalidd Input, this endpoint only accepts verified cancellation requests.';
-        console.log('webhook req called');
-        stripe.events.retrieve(req.body.id, (err, event) => {
-            if (err) return res.status(400).json({
+router.post('/stripe', (req, res) => {
+    const errMsg = 'Invalidd Input, this endpoint only accepts verified cancellation requests.';
+    console.log('webhook req called');
+    stripe.events.retrieve(req.body.id, (err, event) => {
+        if (err) return res.status(400).json({
+            message: errMsg
+        });
+        console.log(req.body);
+        console.log(req.query);
+        if (event.type !== 'customer.subscription.deleted') {
+            return res.status(200).json({
                 message: errMsg
             });
-            console.log(req.body);
-            console.log(req.query);
-            if (event.type !== 'customer.subscription.deleted') {
-                return res.status(200).json({
-                    message: errMsg
-                });
-            } else {
-                return res.status(200).json({
-                    message: errMsg
-                });
-            }
-        });
+        } else {
+            return res.status(200).json({
+                message: errMsg
+            });
+        }
     });
-    return router;
-}
+});
+
+export default router;
